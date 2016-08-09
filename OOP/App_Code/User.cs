334071f9c924bfe0ae883adf7e127030ae641a56
Plaitrix <jens.suldrup@gmail.com>
2016-08-09 10:46:53 +0200

    commit

---
+++ OOP/App_Code/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserClass
+{
+
+/// <summary>
+/// Summary description for User
+/// </summary>
+public class User
+{
+        public static bool IsUser()
+        {
+            // en forkortede udgave af stumpen nede under
+            //return HttpContext.Current.Session["user"] != null ? true : false;
+
+
+            bool result = false;
+
+            if (HttpContext.Current.Session["user"] != null)
+            {
+                result = true;
+            }
+
+            return result;
+        }
+
+    protected void Page_Load(object sender,EventArgs eq)
+
+}
+
+}
---
+++ OOP/TestBruger.aspx
@@ -0,0 +1,16 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="TestBruger.aspx.cs" Inherits="TestBruger" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lbl_user" runat="server" Text=""></asp:Label>
+    </div>
+    </form>
+</body>
+</html>

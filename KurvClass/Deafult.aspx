fa1257ba67e88f0489a61f34adcebb9247345b1b
Plaitrix <jens.suldrup@gmail.com>
2016-08-09 12:30:25 +0200

    Add functions when you add items to shop

---
+++ KurvClass/Deafult.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Deafult.aspx.cs" Inherits="KurvClass.Deafult" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>ID</td>
+                <td> <asp:TextBox ID="TextBox_id" runat="server"></asp:TextBox> </td>
+            </tr>
+            <tr>
+                <td>Navn</td>
+                <td> <asp:TextBox ID="TextBox_name" runat="server"></asp:TextBox> </td>
+            </tr>
+            <tr>
+                <td>Pris</td>
+                <td> <asp:TextBox ID="TextBox_price" runat="server"></asp:TextBox> </td>
+            </tr>
+            <tr>
+                <td>Antal</td>
+                <td> <asp:TextBox ID="TextBox_amount" runat="server"></asp:TextBox> </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Button ID="Button_submit" Text="Put i kurv" OnClick="Button_submit_Clicked" runat="server" />
+                </td>
+            </tr>
+        </table>
+
+        <asp:GridView ID="GV_cart" runat="server"></asp:GridView>
+
+    </div>
+    </form>
+</body>
+</html>
---
+++ KurvClass/Deafult.aspx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace KurvClass
+{
+    public partial class Deafult : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button_submit_Clicked(object sender, EventArgs e)
+        {
+            List<CartProduct> cart = new List<CartProduct>();
+
+            cart = ShopCart(cart);
+
+            addToCart(cart);
+
+            ShowCart(cart);
+        }
+
+        private void ShowCart(List<CartProduct> cart)
+        {
+            GV_cart.DataSource = cart;
+            GV_cart.DataBind();
+        }
+
+        private void addToCart(List<CartProduct> cart)
+        {
+            bool newProduct = true;
+
+            foreach (CartProduct product in cart)
+            {
+                if (product.Id == Convert.ToInt32(TextBox_id.Text))
+                {
+                    newProduct = false;
+                    product.Amount += Convert.ToInt32(TextBox_amount.Text);
+                    product.TotalPrice = product.Amount * product.Price;
+                }
+            }
+            if (newProduct)
+            {
+                cart.Add(new CartProduct(
+                    Convert.ToInt32(TextBox_id.Text),
+                    TextBox_name.Text,
+                    Convert.ToDecimal(TextBox_price.Text),
+                    Convert.ToInt32(TextBox_amount.Text)
+                    ));
+            }
+        }
+
+        private List<CartProduct> ShopCart(List<CartProduct> cart)
+        {
+            if (Session["Cart"] == null)
+            {
+                Session.Add("Cart", cart);
+            }
+
+            cart = Session["Cart"] as List<CartProduct>;
+            return cart;
+        }
+    }
+}
